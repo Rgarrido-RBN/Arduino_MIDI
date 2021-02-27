9c09c3b763c765ca3d9b65ab0205ead557badac4
Rgarrido-RBN <rgarrido.rbn@gmail.com>
2021-02-27 20:20:39 +0100

    First code update

--- arduino_midi/arduino_midi.ino
+++ arduino_midi/arduino_midi.ino
@@ -1,46 +1,37 @@
-/*
-  MIDI note player
+/* 
+  Created 26 Feb 2021
+  by Rubén Garrido
 
-  This sketch shows how to use the serial transmit pin (pin 1) to send MIDI note data.
-  If this circuit is connected to a MIDI synth, it will play the notes
-  F#-0 (0x1E) to F#-5 (0x5A) in sequence.
-
-  The circuit:
-  - digital in 1 connected to MIDI jack pin 5
-  - MIDI jack pin 2 connected to ground
-  - MIDI jack pin 4 connected to +5V through 220 ohm resistor
-  - Attach a MIDI cable to the jack, then to a MIDI synth, and play music.
-
-  created 13 Jun 2006
-  modified 13 Aug 2012
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Midi
+  Arduino midi controller with Arduino Mega 2560 
+  
 */
 
+#include <MIDI.h>
+
+#define NUMOFBUTTONS  6
+
+MIDI_CREATE_DEFAULT_INSTANCE();
+// Digital Pins usables for interrupts 2, 3, 18, 19, 20, 21
+
+const int button[NUMOFBUTTONS] = {2,3,18,19,20,21};
+const int led[NUMOFBUTTONS] = {6,7,8,9,10,11};
+
 void setup() {
+  
   // Set MIDI baud rate:
   Serial.begin(31250);
+  
+  //Init pins of Arduino
+  for(int i=0; i<NUMOFBUTTONS; i++){
+  pinMode(button[i], INPUT);
+  pinMode(led[i], OUTPUT);
+  }
+
+  //Init ISR for buttons
+  initISR();
+
 }
 
 void loop() {
-  // play notes from F#-0 (0x1E) to F#-5 (0x5A):
-  for (int note = 0x1E; note < 0x5A; note ++) {
-    //Note on channel 1 (0x90), some note value (note), middle velocity (0x45):
-    noteOn(0x90, note, 0x45);
-    delay(100);
-    //Note on channel 1 (0x90), some note value (note), silent velocity (0x00):
-    noteOn(0x90, note, 0x00);
-    delay(100);
-  }
-}
-
-// plays a MIDI note. Doesn't check to see that cmd is greater than 127, or that
-// data values are less than 127:
-void noteOn(int cmd, int pitch, int velocity) {
-  Serial.write(cmd);
-  Serial.write(pitch);
-  Serial.write(velocity);
+  
 }
