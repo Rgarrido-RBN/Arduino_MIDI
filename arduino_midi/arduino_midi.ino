4c60f3b7846416bc45491cec0f6c7f52e6e66c50
Ruben <rgarrido.rbn@gmail.com>
2021-03-27 11:48:08 +0100

    LCD screen feature added

--- arduino_midi/arduino_midi.ino
+++ arduino_midi/arduino_midi.ino
@@ -1,4 +1,3 @@
-
 /* 
   Created 26 Feb 2021
   by Rubén Garrido
@@ -7,6 +6,7 @@
   
 */
 #include "MIDI.h"
+#include <LiquidCrystal.h>
 
 #define NUMOFBUTTONS  6
 
@@ -14,13 +14,16 @@
 
 const int button[NUMOFBUTTONS] = {2,3,18,19,20,21};
 const int led[NUMOFBUTTONS] = {6,7,8,9,10,11};
+int prevPreset = 0;
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-void setup() {
-  
+void setup() 
+{
   Serial.begin(9600);
   initPins();
   initISR();
-
+  lcd.begin(16, 2);
 }
 
 void loop() 
