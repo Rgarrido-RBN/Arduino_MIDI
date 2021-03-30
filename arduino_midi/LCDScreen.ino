5d93930e06e275b11f2a75239f66769249cc743c
Ruben <rgarrido.rbn@gmail.com>
2021-03-30 22:41:57 +0200

    LCDScreen file addded

---
+++ arduino_midi/LCDScreen.ino
@@ -0,0 +1,14 @@
+/* 
+  Created 26 Feb 2021
+  by Rubén Garrido
+
+  Arduino MIDI controller using Arduino Mega 2560 
+  
+*/
+
+void lcdScreen(unsigned char presetName)
+{
+lcd.print(presetname);
+}
+
+
